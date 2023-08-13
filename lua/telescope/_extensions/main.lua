590fdc98db477292259252860a248fc94b63f55c
Aayush <84364760+archelaus@users.noreply.github.com>
2023-08-13 10:39:07 +0530

    Update main.lua

--- lua/telescope/_extensions/main.lua
+++ lua/telescope/_extensions/main.lua
@@ -1,5 +1,10 @@
-return require("telescope").register_extension({
+local scripts = function()
+  local telescope_undo = require("telescope-scripts")
+  telescope_scripts.scripts()
+end
+
+return telescope.register_extension({
   exports = {
-    scripts = scripts
+    scrripts = scripts,
   },
 })
