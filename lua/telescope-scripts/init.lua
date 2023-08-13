024b504d0e48bdc2bc8ddc74062bacab461c3fd8
Aayush <84364760+archelaus@users.noreply.github.com>
2023-08-13 10:31:59 +0530

    Update init.lua

--- lua/telescope-scripts/init.lua
+++ lua/telescope-scripts/init.lua
@@ -1,4 +1,4 @@
-require('telescope').load_extension('telescope-vim-scripts')
+require('telescope').load_extension('telescope-scripts')
 
 local telescope_actions = require('telescope.actions.vim_script_actions')
 local telescope_pickers = require('telescope.pickers.vim_script_pickers')
